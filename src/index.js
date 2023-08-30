aa7b0cfd27b389749e7e4c0f63d8ca55595414d3
Aleksandr232 <aleksrowing232@gmail.com>
2023-08-30 23:33:33 +0300

    index

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createRoot } from 'react-dom/client';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from "./redux/reducers";
 import App from "./App";
+
+
 
 const store = createStore(reducer, compose(
   applyMiddleware(thunk),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() 
 ));
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+root.render(
   <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
+
